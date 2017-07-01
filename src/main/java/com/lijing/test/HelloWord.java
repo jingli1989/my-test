5ab12d38edf54baec80963a61d235c98f4b280fb
lijing <545903902@qq.com>
2017-07-01 16:49:05 +0800

    test

---
+++ src/main/java/com/lijing/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.lijing.test;
+
+/**
+ * Created by Administrator on 2017/7/1 0001.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HellWord!");
+    }
+}
